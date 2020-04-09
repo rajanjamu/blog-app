96fe5e7157242dda92d952ff8aada3e3ca67a808
rajanjamu <rajanjamu@gmail.com>
2020-04-10 00:45:32 +0530

    added user reference to post model

--- models/post.js
+++ models/post.js
@@ -1,10 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose  = require('mongoose');
+const User      = require('./user');
 
 // Declare the DB Schema
 const postSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: User
+        },
+        username: String
+    },
     created: { type: Date, default: Date.now }
 });
 
